1ede671782527f2871ab9baa8d9486f4c2bac332
David A <readytowin2298@gmail.com>
2021-03-18 22:14:12 -0500

    finished class, working on script

--- makeText.js
+++ makeText.js
@@ -1,2 +1,18 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const argv = process.argv;
+const MarkovMachine = require('./markov.js');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const web = argv[2].indexOf('http') === 0 ? true : false;
+
+if(web){
+    const url = argv[2]
+    axios.get(url).then(data =>{
+
+    })
+} else{
+
+}
--- markov.js
+++ markov.js
@@ -8,7 +8,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.chains = this.makeChains();
   }
 
   /** set markov chains:
@@ -17,13 +17,42 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map()
+    for(let i=0; i<this.words.length; i++){
+      let word = this.words[i]
+      if(this.words[i+1]){
+        if(!chains.has(word)){
+          chains.set(word, [this.words[i+1]])
+        }
+        else{
+          let arr = chains.get(word)
+          arr.push(this.words[i+1])
+          chains.set(word, arr)
+        }
+      }
+      
+    }
+    return (chains)
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let story = ""
+    let word = this.words[Math.floor(Math.random() * this.words.length)]
+    story += word
+    for(let i=0; i<numWords; i++){
+      story += ' '
+      let arr = this.chains.get(word)
+      let newWord = arr[Math.floor(Math.random() * arr.length)]
+      story += newWord
+      word = newWord
+    }
+    return story
   }
+}
+
+module.exports = {
+  MarkovMachine : MarkovMachine
 }
